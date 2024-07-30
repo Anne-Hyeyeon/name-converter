008f47628af5ecc9635290e350edeceed53452b2
Hyeyeon(Anne) <khy2106@naver.com>
2024-07-30 23:48:00 +0900

    Feat: 0730 이름 업데이트 추가

---
+++ src/app/data/20240730.ts
@@ -0,0 +1,20 @@
+export interface Update {
+ date: string;
+ patchNotes: string[];
+ names: string[];
+}
+
+export const updateData: Update = {
+ date: "2024-07-30",
+ patchNotes: [
+  "데이터가 없을 때 보이는 화면이 변경되었습니다. 찾는 이름에 대해 더 자세한 안내를 받을 수 있어요.",
+ ],
+ names:
+  "RiaAspynAlizahCillianHaydenErenRinaEllyEidanFraserGabbyMaddyHoneyLeenaCaesarNessaAshNateElodieRoxyKimmyDanyaGiseleLaniYunaSamiTrixieBeccaSereneDoriePiaCateIrinaDixonGloryDoraAleynaBeatrixTallulahDyaneHalseyLita".match(
+   /[A-Z][a-z]+/g
+  ) || [],
+};
+
+export function getUpdateData(): Update {
+ return updateData;
+}
--- src/app/utils/getAllUpdateData.ts
+++ src/app/utils/getAllUpdateData.ts
@@ -1,4 +1,5 @@
 import { getUpdateData as getUpdateData20240729 } from "../data/20240729";
+import { getUpdateData as getUpdateData20240730 } from "../data/20240730"; // 새로운 import 추가
 
 export interface Update {
  date: string;
@@ -7,7 +8,7 @@ export interface Update {
 }
 
 export function getAllUpdateData(): Update[] {
- return [getUpdateData20240729()].sort(
+ return [getUpdateData20240729(), getUpdateData20240730()].sort(
   (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
  );
 }
