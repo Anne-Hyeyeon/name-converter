442e90a8288a8a62875f038f1f77224f46cbf487
hykim <khy2106@naver.com>
2024-07-30 09:43:22 +0900

    Feat: Google AdSense 스크립트 추

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,39 +3,48 @@ import "./globals.css";
 import styles from "./layout.module.css";
 import Footer from "./components/Footer";
 import CoupangBanner from "./components/CoopangBanner";
+import Script from "next/script";
 
 export const metadata: Metadata = {
- title: "내가 춘자라니",
- description:
-  "Anne이 미국에선 춘자라고? 내가 춘자라니! 내 영어 이름이 미국에서 촌스러운 이름인지, 트렌디한 이름인지 알아보기! 이름의 유행 연도도 알 수 있어요ㅎㅎ",
- openGraph: {
-  type: "website",
-  url: "https://chunja.vercel.app",
   title: "내가 춘자라니",
   description:
-   "Anne이 미국에선 춘자라고? 내가 춘자라니! 내 영어 이름이 미국에서 촌스러운 이름인지, 트렌디한 이름인지 알아보기! 이름의 유행 연도도 알 수 있어요ㅎㅎ",
- },
- icons: {
-  icon: "/favicon.ico",
-  shortcut: "/favicon-16x16.png",
-  apple: "/apple-touch-icon.png",
- },
+    "Anne이 미국에선 춘자라고? 내가 춘자라니! 내 영어 이름이 미국에서 촌스러운 이름인지, 트렌디한 이름인지 알아보기! 이름의 유행 연도도 알 수 있어요ㅎㅎ",
+  openGraph: {
+    type: "website",
+    url: "https://chunja.vercel.app",
+    title: "내가 춘자라니",
+    description:
+      "Anne이 미국에선 춘자라고? 내가 춘자라니! 내 영어 이름이 미국에서 촌스러운 이름인지, 트렌디한 이름인지 알아보기! 이름의 유행 연도도 알 수 있어요ㅎㅎ",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
- children,
+  children,
 }: {
- children: React.ReactNode;
+  children: React.ReactNode;
 }) {
- return (
-  <html lang="ko">
-   <body className={styles.body}>
-    <main className={styles.main}>{children} </main>
-    <Footer />
-    <div className={styles.banner}>
-     <CoupangBanner />
-    </div>
-   </body>
-  </html>
- );
+  return (
+    <html lang="ko">
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-xxxxxxxxxxxxxxxx"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
+      <body className={styles.body}>
+        <main className={styles.main}>{children}</main>
+        <Footer />
+        <div className={styles.banner}>
+          <CoupangBanner />
+        </div>
+      </body>
+    </html>
+  );
 }
