1b356f1ed6186330104914d71794165c7418fe59
Hyeyeon(Anne) <khy2106@naver.com>
2024-07-31 00:18:38 +0900

    Fix: 구글 애널리틱스 속성 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import styles from "./layout.module.css";
 import Footer from "./components/Footer";
 import CoupangBanner from "./components/CoopangBanner";
+import Script from "next/script";
 
 export const metadata: Metadata = {
  title: "내가 춘자라니",
@@ -21,7 +22,6 @@ export const metadata: Metadata = {
   apple: "/apple-touch-icon.png",
  },
 };
-
 export default function RootLayout({
  children,
 }: {
@@ -30,37 +30,21 @@ export default function RootLayout({
  return (
   <html lang="ko">
    <head>
-    {/* Google Tag Manager */}
-    <script
-     dangerouslySetInnerHTML={{
-      __html: `
-            (function(w,d,s,l,i){
-              w[l]=w[l]||[];
-              w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WJ4RGKP8');
-            `,
-     }}
+    <Script
+     src="https://www.googletagmanager.com/gtag/js?id=G-M1E6F65P9K"
+     strategy="afterInteractive"
     />
-    {/* End Google Tag Manager */}
+    <Script id="google-analytics" strategy="afterInteractive">
+     {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-M1E6F65P9K');
+          `}
+    </Script>
    </head>
    <body className={styles.body}>
-    {/* Google Tag Manager (noscript) */}
-    <noscript>
-     <iframe
-      src="https://www.googletagmanager.com/ns.html?id=GTM-WJ4RGKP8"
-      height="0"
-      width="0"
-      style={{ display: "none", visibility: "hidden" }}
-     />
-    </noscript>
-    {/* End Google Tag Manager (noscript) */}
-    <main className={styles.main}>{children} </main>
+    <main className={styles.main}>{children}</main>
     <Footer />
     <div className={styles.banner}>
      <CoupangBanner />
