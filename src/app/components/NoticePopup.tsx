42a9947371cbf5196dd73a192a6af810db74b748
Hyeyeon(Anne) <khy2106@naver.com>
2024-08-01 23:27:24 +0900

    Fix: 팝업 문구 수정

--- src/app/components/NoticePopup.tsx
+++ src/app/components/NoticePopup.tsx
@@ -11,22 +11,26 @@ const NoticePopup: React.FC<NoticePopupProps> = ({ onClose }) => {
    <div className={styles.modal}>
     <h2 className={styles.title}>춘자 사이트를 사랑해주시는 여러분! 😭 </h2>
     <div className={styles.content}>
-     <p>제곧내인데요... 춘자 사이트가 없어질 위기에 처해 있습니다... 😢</p>
+     <p className={styles.stats}>
+      <strong>
+       제곧내인데요... 춘자 사이트가 없어질 위기에 처해 있습니다... 😢
+      </strong>
+     </p>
 
      <p>
-      사이트가 많이 알려진 덕분에, 방문자들이 증가하고 있지만
+      사이트가 많이 알려진 덕분에 방문자들이 증가하고 있지만
       <br />
-      그만큼 유지비가 증가하여 개발자의 아주 알량한... 용돈으로 충당
-      중이었습니다. 🙀
+      그만큼 유지비가 증가하여 개발자의 아주 알량한...🥺 용돈으로 충당하기가
+      어려워지고 있습니다.
      </p>
 
      <p>
       이대로 가다가는 사이트가 오래 유지되기 어려울 것 같아
       <br />
-      여러분에게 도움을 청합니다! 🙏
+      여러분에게 도움을 청합니다!
      </p>
 
-     <div className={`${styles.content} ${styles.highlightSection}`}>
+     <div className={styles.highlightSection}>
       <p className={styles.highlight}>
        💖 메인의 사진이나, 결과 페이지의 &apos;개발자 응원하기&apos;를 눌러
        <br />
@@ -44,14 +48,17 @@ const NoticePopup: React.FC<NoticePopupProps> = ({ onClose }) => {
       </p>
      </div>
 
-     <strong>
-      &apos;춘자&apos;는 일 방문자 5만, <br />
-      (8/1 10pm 기준) 분당 동시 접속자 수 700명 대의 사이트예요. 🚀
-     </strong>
+     <p className={styles.stats}>
+      <strong>
+       &apos;춘자&apos;는 일 방문자 5만, <br />
+       (8/1 10pm 기준) 분당 동시 접속자 수 700명 대의 사이트예요.
+      </strong>
+     </p>
 
      <p>커피 기부, 광고 게시에 많은 관심 부탁드립니다!</p>
 
-     <p>부디 춘자 사이트가 롱런하는 데에 도움을 주세요! 🙌</p>
+     <p>부디 춘자 사이트가 롱런하는 데에 도움을 주세요! </p>
+
      <a
       href="https://www.buymeacoffee.com/annehyeyeon"
       target="_blank"
