8633873938e17b5984b2234a3dc180212cb1fe74
Hyeyeon(Anne) <khy2106@naver.com>
2024-08-04 01:57:21 +0900

    Feat: 0804 업데이트 노트 추가

--- src/app/utils/getAllUpdateData.ts
+++ src/app/utils/getAllUpdateData.ts
@@ -1,14 +1,12 @@
 import { getUpdateData as getUpdateData20240729 } from "../data/20240729";
-import { getUpdateData as getUpdateData20240730 } from "../data/20240730"; // 새로운 import 추가
-
-export interface Update {
- date: string;
- patchNotes: string[];
- names: string[];
-}
+import { getUpdateData as getUpdateData20240730 } from "../data/20240730";
+import { getUpdateData as getUpdateData20240804 } from "../data/20240804";
+import { Update } from "../types";
 
 export function getAllUpdateData(): Update[] {
- return [getUpdateData20240729(), getUpdateData20240730()].sort(
-  (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
- );
+ return [
+  getUpdateData20240729(),
+  getUpdateData20240730(),
+  getUpdateData20240804(),
+ ].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 }
