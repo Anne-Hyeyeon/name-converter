a35cee7e145e25bbe5b0e44117982904f6239d18
hykim <khy2106@naver.com>
2024-08-07 11:29:31 +0900

    Refactor: 메시지와 이모지 별도 파일로 분리
    - characteristicMessages와 emojiMap을 constants/resultMessages.ts로 이동
    - 각 메시지와 이모지에 해당하는 특성에 대한 주석 추가

--- src/app/components/SearchResult.tsx
+++ src/app/components/SearchResult.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import React from "react";
-import { NameData } from "../types";
-import styles from "./SearchResult.module.css";
 import html2canvas from "html2canvas";
 import { useRouter } from "next/navigation";
+import { NameData } from "../types";
+import { characteristicMessages, emojiMap } from "../constants/resultMessages";
+import styles from "./SearchResult.module.css";
 
 export const generateMetadata = ({ result }: { result: NameData }) => {
   const title = `이름 검색 결과 : ${result.name}은 촌스러운 이름일까?`;
@@ -14,59 +15,6 @@ export const generateMetadata = ({ result }: { result: NameData }) => {
     title,
     description,
   };
-};
-
-const characteristicMessages = [
-  [
-    "20세기 초반에 유행했던 이름이에요!",
-    "혹시.. 역사적인 인물이세요?",
-    "아니면 타임머신 타고 오셨나요? 🕰️",
-  ],
-  ["'베이비 부머' 세대 이름이에요!", "Peace! ✌️ 히피 느낌이 낭낭하네요!"],
-  [
-    "X세대에서 많이 사용된 이름이에요!",
-    "카세트테이프가 있던 낭만의 시절! 🎸📼",
-  ],
-  [
-    "밀레니얼 세대에서 주로 사용하는 이름이에요!",
-    "80~90년대생이라면",
-    "이 이름이 완전 찰떡이라고 할 수 있죠!🕺",
-  ],
-  [
-    "엄마들한테 인기가 많은 Z세대 이름이에요!",
-    "틱톡, 인스타그램 스토리 감성 그 자체! 📱💃",
-  ],
-  [
-    "예전부터 지금까지 꾸준히 사랑받으면서,",
-    "클래식함과 트렌디함이 공존하는 이름이에요!🤩",
-  ],
-  [
-    "앗! 이 이름은 강아지 이름으로 더 자주 쓰여요 🥺",
-    "귀여운 강아지에게 양보하세요! 🐕",
-  ],
-  [
-    "이 이름은 미국이 아닌",
-    "아프리카, 유럽 등의 지역에서 유래된 이름이에요.",
-    "독특한 문화적 배경을 가진 멋진 이름이네요!",
-    "이름의 고유한 의미와 아름다움을",
-    "그대로 간직해보는 건 어떨까요? 🌍✨",
-  ],
-  [
-    "이 이름은 아시아 사람들이",
-    "본명을 따서 지었을 가능성이 커요.",
-    "이름의 고유한 의미와 아름다움을",
-    "그대로 간직해보는 건 어떨까요? 🌍✨",
-  ],
-  [
-    "검색하신 이름은, 결과를 보여드리기 어려운 이름이에요.",
-    "그럼에도 불구하고, 많은 분들이 검색한 이름이라",
-    "설명을 위해 데이터에 추가했답니다.",
-  ],
-];
-
-const emojiMap = {
-  F: ["👵", "👩", "👩‍🦰", "👩‍🦱", "👧", "👧", "🐩", "👩‍🦱", "👩", "🤷‍♀️"],
-  M: ["👴", "👨", "👨‍🦰", "👨‍🦱", "👦", "👦", "🐕", "👱‍♂️", "👨", "🤷‍♂️"],
 };
 
 interface SearchResultProps {
