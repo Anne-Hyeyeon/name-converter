8f93d5c6ebbf500dfa87ad8464c4db0b0bbfe2b3
Hyeyeon(Anne) <khy2106@naver.com>
2024-08-18 23:09:00 +0900

    Feature: Add Adrop SDK

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,50 +6,63 @@ import CoupangBanner from "./components/common/CoopangBanner";
 import Script from "next/script";
 
 export const metadata: Metadata = {
+ title: "내가 춘자라니",
+ description:
+  "Anne이 미국에선 춘자라고? 내가 춘자라니! 내 영어 이름이 미국에서 촌스러운 이름인지, 트렌디한 이름인지 알아보기! 이름의 유행 연도도 알 수 있어요ㅎㅎ",
+ openGraph: {
+  type: "website",
+  url: "https://chunja.vercel.app",
   title: "내가 춘자라니",
   description:
-    "Anne이 미국에선 춘자라고? 내가 춘자라니! 내 영어 이름이 미국에서 촌스러운 이름인지, 트렌디한 이름인지 알아보기! 이름의 유행 연도도 알 수 있어요ㅎㅎ",
-  openGraph: {
-    type: "website",
-    url: "https://chunja.vercel.app",
-    title: "내가 춘자라니",
-    description:
-      "Anne이 미국에선 춘자라고? 내가 춘자라니! 내 영어 이름이 미국에서 촌스러운 이름인지, 트렌디한 이름인지 알아보기! 이름의 유행 연도도 알 수 있어요ㅎㅎ",
-  },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
+   "Anne이 미국에선 춘자라고? 내가 춘자라니! 내 영어 이름이 미국에서 촌스러운 이름인지, 트렌디한 이름인지 알아보기! 이름의 유행 연도도 알 수 있어요ㅎㅎ",
+ },
+ icons: {
+  icon: "/favicon.ico",
+  shortcut: "/favicon-16x16.png",
+  apple: "/apple-touch-icon.png",
+ },
 };
+
 export default function RootLayout({
-  children,
+ children,
 }: {
-  children: React.ReactNode;
+ children: React.ReactNode;
 }) {
-  return (
-    <html lang="ko">
-      <head>
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-M1E6F65P9K"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
+ return (
+  <html lang="ko">
+   <head>
+    <Script
+     src="https://www.googletagmanager.com/gtag/js?id=G-M1E6F65P9K"
+     strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+     {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-M1E6F65P9K');
           `}
-        </Script>
-      </head>
-      <body className={styles.body}>
-        <main className={styles.main}>{children}</main>
-        <Footer />
-        <div className={styles.banner}>
-          <CoupangBanner />
-        </div>
-      </body>
-    </html>
-  );
+    </Script>
+    <Script
+     src="https://storage.adrop.io/js/adrop-0.1.2.min.js"
+     strategy="lazyOnload"
+     onLoad={() => {
+      console.log("Adrop script loaded successfully");
+      window.adrop = window.adrop || {};
+      window.adrop.initialize("ADROP_APP_KEY"); // 여기에 실제 AppKey를 입력하세요
+     }}
+     onError={(e) => {
+      console.error("Error loading Adrop script", e);
+     }}
+    />
+   </head>
+   <body className={styles.body}>
+    <main className={styles.main}>{children}</main>
+    <Footer />
+    <div className={styles.banner}>
+     <CoupangBanner />
+    </div>
+   </body>
+  </html>
+ );
 }
