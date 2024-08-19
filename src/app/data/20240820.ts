deb6072151f16b597472fb88ac328d68d7bba83e
Hyeyeon(Anne) <khy2106@naver.com>
2024-08-20 00:55:28 +0900

    Fix: 업데이트 노트 날짜 수정

--- src/app/data/20240820.ts
+++ src/app/data/20240820.ts
@@ -1,7 +1,7 @@
 import { Update } from "../types";
 
 export const updateData: Update = {
- date: "2024-08-04",
+ date: "2024-08-20",
  patchNotes: ["휴가를 다녀 오느라 업데이트가 많이 늦었어요 🥺"],
  names:
   "AvaFeliciaKristineYvesSweeneyJennMashaDollyLindenNatiaJenHanniEnoshAdoraRoaYesicaRayleeJerushaJervisEliaIainNarshaNiya".match(
