ca72ef045601216de905eb7d54746fdea5ef418a
Hyeyeon(Anne) <khy2106@naver.com>
2024-08-20 00:53:58 +0900

    Feat: 0820 업데이트 노트 추가

---
+++ src/app/data/20240820.ts
@@ -0,0 +1,14 @@
+import { Update } from "../types";
+
+export const updateData: Update = {
+ date: "2024-08-04",
+ patchNotes: ["휴가를 다녀 오느라 업데이트가 많이 늦었어요 🥺"],
+ names:
+  "AvaFeliciaKristineYvesSweeneyJennMashaDollyLindenNatiaJenHanniEnoshAdoraRoaYesicaRayleeJerushaJervisEliaIainNarshaNiya".match(
+   /[A-Z][a-z]+/g
+  ) || [],
+};
+
+export function getUpdateData(): Update {
+ return updateData;
+}
--- src/app/utils/getAllUpdateData.ts
+++ src/app/utils/getAllUpdateData.ts
@@ -1,12 +1,14 @@
 import { getUpdateData as getUpdateData20240729 } from "../data/20240729";
 import { getUpdateData as getUpdateData20240730 } from "../data/20240730";
 import { getUpdateData as getUpdateData20240804 } from "../data/20240804";
+import { getUpdateData as getUpdateData20240820 } from "../data/20240820";
 import { Update } from "../types";
 
 export function getAllUpdateData(): Update[] {
-  return [
-    getUpdateData20240729(),
-    getUpdateData20240730(),
-    getUpdateData20240804(),
-  ].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+ return [
+  getUpdateData20240729(),
+  getUpdateData20240730(),
+  getUpdateData20240804(),
+  getUpdateData20240820(),
+ ].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 }
