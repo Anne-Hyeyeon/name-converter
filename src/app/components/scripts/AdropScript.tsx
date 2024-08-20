f609c11b408020a1893ebf12a11e83eb795de078
Hyeyeon(Anne) <khy2106@naver.com>
2024-08-20 21:39:21 +0900

    Fix: Adrop 스크립트 로딩 오류 수정

--- src/app/components/scripts/AdropScript.tsx
+++ src/app/components/scripts/AdropScript.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Script from "next/script";
+import { useEffect } from "react";
 
 declare global {
  interface Window {
@@ -15,18 +16,44 @@ declare global {
 }
 
 export default function AdropScript() {
+ useEffect(() => {
+  const initializeAdrop = () => {
+   if (window.adrop && typeof window.adrop.initialize === "function") {
+    window.adrop.initialize(`${process.env.NEXT_PUBLIC_ADROP_APP_KEY}`);
+    console.log("Adrop initialized successfully");
+   } else {
+    console.error("Adrop initialize function not found");
+   }
+  };
+
+  if (
+   document.querySelector(
+    'script[src="https://storage.adrop.io/js/adrop-0.1.2.min.js"]'
+   )
+  ) {
+   initializeAdrop();
+  } else {
+   window.addEventListener("adrop-loaded", initializeAdrop);
+  }
+
+  return () => {
+   window.removeEventListener("adrop-loaded", initializeAdrop);
+  };
+ }, []);
+
  return (
-  <Script
-   src="https://storage.adrop.io/js/adrop-0.1.2.min.js"
-   strategy="lazyOnload"
-   onLoad={() => {
-    console.log("Adrop script loaded successfully");
-    window.adrop = window.adrop || {};
-    window.adrop.initialize("ADROP_APP_KEY");
-   }}
-   onError={(e) => {
-    console.error("Error loading Adrop script", e);
-   }}
-  />
+  <>
+   <Script
+    id="adrop-main"
+    src="https://storage.adrop.io/js/adrop-0.1.2.min.js"
+    onLoad={() => {
+     console.log("Adrop script loaded successfully");
+     window.dispatchEvent(new Event("adrop-loaded"));
+    }}
+    onError={(e) => {
+     console.error("Error loading Adrop script", e);
+    }}
+   />
+  </>
  );
 }
