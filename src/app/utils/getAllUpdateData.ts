f0e6b0c7b76267d11b38a4dbc6ca27c756d1cead
Hyeyeon(Anne) <khy2106@naver.com>
2024-09-24 00:52:35 +0900

    Refactor: 개별 업데이트 파일을 하나의 JSON 파일로 통합

--- src/app/utils/getAllUpdateData.ts
+++ src/app/utils/getAllUpdateData.ts
@@ -1,18 +1,11 @@
-import { getUpdateData as getUpdateData20240729 } from "../data/20240729";
-import { getUpdateData as getUpdateData20240730 } from "../data/20240730";
-import { getUpdateData as getUpdateData20240804 } from "../data/20240804";
-import { getUpdateData as getUpdateData20240820 } from "../data/20240820";
-import { getUpdateData as getUpdateData20240829 } from "../data/20240829";
-import { getUpdateData as getUpdateData20240924 } from "../data/20240924";
+import updatesData from "../data/updates.json";
 import { Update } from "../types";
 
 export function getAllUpdateData(): Update[] {
- return [
-  getUpdateData20240729(),
-  getUpdateData20240730(),
-  getUpdateData20240804(),
-  getUpdateData20240820(),
-  getUpdateData20240829(),
-  getUpdateData20240924(),
- ].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+ return updatesData
+  .map((update) => ({
+   ...update,
+   names: update.names.match(/[A-Z][a-z]+/g) || [],
+  }))
+  .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
 }
